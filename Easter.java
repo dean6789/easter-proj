5d58a97d121f48cf067b7def59043949219e9f99
dean6789 <173644@mcpsmd.net>
2026-09-04 14:19:41 +0000

    int h

--- Easter.java
+++ Easter.java
@@ -9,6 +9,23 @@ public class Easter{
         int y = scanner.nextInt(); // reads the next number the user types. 
         int a = y % 19;
         System.out.println("a = "+a);
+        int b = y / 100;
+        System.out.println("b= "+b);
+        int c = y % 100;
+        System.out.println("c = "+c);
+        int d = b / 4;
+        System.out.println("d = "+d);
+        int e = b % 4;
+        System.out.println("e = "+e);
+        int f = b + 8 / 25; 
+        System.out.println("f = "+f);
+        int g = b - f + 1 / 3;
+        System.out.println("g = "+g);
+        int h = (19 * a + b - d - g + 15) / 30;
+        System.out.println("h = "+h);
+
+
+
     }
 }
 
